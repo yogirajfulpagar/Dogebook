4fec950c43870441faea91ba2ec9081965ec6592
Yogiraj <yogirajfulpagar9193@gmail.com>
2023-05-13 20:36:05 +0530

    third commit

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${_id}/${friendId}`,
+      `https://sociopedia-n1k9.onrender.com/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`https://sociopedia-n1k9.onrender.com/assets/${image}`}
       />
     </Box>
   );
